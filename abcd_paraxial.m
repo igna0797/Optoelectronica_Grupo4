608488de63b827fd750082476e55d6699b943a7f
igna0797 <72945854+igna0797@users.noreply.github.com>
2021-05-02 19:39:21 -0300

    Add files via upload

---
+++ abcd_paraxial.m
@@ -0,0 +1,48 @@
+clc
+clear all 
+close all
+
+##pkg load optics
+##pkg load symbolic
+
+%primera lente a 50mm
+## f1=1
+## f2=2
+## n1=1
+## n2=1
+## L1=50e-3
+## L2=200e-3
+
+ syms f1 f2 L2 L1;
+M1=[[1 L1];[ 0 1] ];
+m21= [1 0];
+m22=[-1/f1 1];
+M2=[m21 ; m22];
+##M2=[1 0 ; -1/f1 1]
+m31= [1 L2];
+M3=[m31 ; [0 1]];
+m42=[-1/f2 1];
+M4 = [[1 0] ; m42];
+ABCD=M4*M3*M2*M1
+
+##C=-1/f1+(L2/n2/f1+1)*1/f2
+
+l1=50   %todas estan en mm
+
+##f=-l1/f1+1-(l1+(l1+l2)*(-l1/f1+1))/f2
+
+## A=1-L1/f1/n1;
+##
+A=1/25e-3
+##f2=[-6 -9 -12 -15 -18  -24 -25]
+f2=-6
+##f1=-f2*A#800
+f1=250
+L2=f1+f2
+s=abcd('propagation',l1,'thin-lens',f1,'propagation',L2,'thin-lens',f2,'propagation',25)
+##
+rin=[0 , 1 , 0.5 ;0 0 0]
+rout=trace(s,rin,true)
+%gauusseano hacce mas ancho segun f (lo queres mas pequeño)
+
+
---
+++ ajuste.m
@@ -0,0 +1,42 @@
+clc
+close all
+clear all
+
+img01 = im2double(rgb2gray(imread("mediciones_tp1/haz_305mm.jpg")));
+
+figure();
+imshow(img01);
+axis("tic", "label");
+
+for i = 1:rows(img01)
+    x_umbrales01 = find(img01(i,:) > 0.6);
+    if (length(x_umbrales01) >= 2)
+        ancho01(i,1) = x_umbrales01(end) - x_umbrales01(1);
+    else
+        ancho01(i,1) = 0;
+    endif
+endfor
+[~, y_max_ancho01] = max(ancho01);
+
+for i = 1:columns(img01)
+    y_umbrales01 = find(img01(:,i) > 0.7);
+    if (length(y_umbrales01) >= 2)
+        alto01(1,i) = y_umbrales01(end) - y_umbrales01(1);
+    else
+        alto01(1,i) = 0;
+    endif
+endfor
+[~, x_max_ancho01] = max(alto01);
+
+% se puede hacer mejor la deteccion del centro
+
+
+figure(2);
+hold on;
+imshow(gray2ind(img01), jet());
+plot(1:(columns(img01)), y_max_ancho01*ones(1,columns(img01)), "-k", "linewidth", 2);
+plot( x_max_ancho01*ones(rows(img01),1),1:(rows(img01)), "-k", "linewidth", 2);
+
+axis("tic", "label");
+xlabel("x / px");
+ylabel("y / px");
