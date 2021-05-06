53b66799083bcb98fe99e9c2596739b788e36aa1
Ignacio Dominguez <igna0797@gmail.com>
2021-05-06 16:03:27 -0300

    Update q.m

--- q.m
+++ q.m
@@ -17,9 +17,11 @@ q_s=1./((C.+D/q_e)./(A.+B/q_e));
 w=sqrt((-imag(1./q_s)).^(-1)*lambda_o/pi);
 w=w.*1e6;
 z=z.*1e3;
-figure()
-plot(z,w,'-r');
+load w_analitico
+fig= figure()
 hold on
+plot(z,w,'-r','linewidth',4);
+plot (z,wpromedio,'--b')
 plot(300,466.5,'r','Markersize',20);
 plot(305,465.4,'g','Markersize',20);
 plot(310,487.6,'c','Markersize',20);
@@ -27,3 +29,4 @@ grid on
 xlabel('z [mm]');
 ylabel('w [\mum]');
 axis([0 350 440 510])
+legend ('w calculado con q' , ' w analitico')
