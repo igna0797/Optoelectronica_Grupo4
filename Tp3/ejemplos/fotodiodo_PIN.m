ab86ccc7e796ed8c3af5ba0e17ab654d917be7f6
Ignacio Dominguez <igna0797@gmail.com>
2021-06-18 11:11:29 -0300

    Create fotodiodo_PIN.m

---
+++ Tp3/ejemplos/fotodiodo_PIN.m
@@ -0,0 +1,12 @@
+%datasheet fotodiodo
+S_min=0.85 %1550 [A/W]
+S=1
+I_d=0.3 %corriente de oscuridad [nA]
+I_d_max=1.5
+fc=300e6  %frecuencia de corte [Hz]
+fc_min=150e6
+G=1 %ganancia
+NEP=5e-15 %[W/Hz]
+NEP_max=2e-14
+D_min=1e12 %directividad especifica [m sqrt(HZ)/W]
+D=5e12
