82708a6c9005eded9d5712b19214f0e34bf585a7
Ignacio Dominguez <igna0797@gmail.com>
2021-06-18 11:11:18 -0300

    Create fotodiodo_APD.m

---
+++ Tp3/ejemplos/fotodiodo_APD.m
@@ -0,0 +1,12 @@
+%datasheet fotodiodo
+S_min=0.65 %1550 [A/W]
+S=0.8
+I_d=20 %corriente de oscuridad [nA]
+I_d_max=50
+fc=0.9e9  %frecuencia de corte [Hz]
+fc_min=0.9e9
+G=30 %ganancia ,-30dBm
+NEP=%[W/Hz]
+NEP_max=
+D_min= %directividad especifica [m sqrt(HZ)/W]
+D=
